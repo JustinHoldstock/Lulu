6234d4975adaa97463b59cafa3c3edf5898954e3
Justin Holdstock <justinholdstock91@gmail.com>
2022-04-29 21:48:54 -0600

    chore: generate sandbox app

---
+++ apps/sandbox/jest.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'sandbox',
+  preset: '../../jest.preset.ts',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  transform: {
+    '^.+\\.[tj]s$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/sandbox',
+};
